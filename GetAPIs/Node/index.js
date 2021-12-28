255f112c834d1d50a29434fd53646c152dcc438e
Devang <risingentropy20@gmail.com>
2021-12-28 21:55:44 +0530

    some

--- GetAPIs/Node/index.js
+++ GetAPIs/Node/index.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const app = express();
 
+const theData = require("./data.json");
+
 app.get("/", (req, res)=>{
-    res.json({"Hello": "World"})
+    res.json({message: "Hello"})
+});
+
+// get all the users
+app.get("/all-users", (req, res)=>{
+    res.json(theData)
+})
+
+// get a particular user
+app.get("/user/:id", (req, res)=>{
+    let id = parseInt(req.params.id);
+    for(let i=0; i<theData.length; i++)
+    {
+        if(theData[i].id === id)
+        {
+            res.json(theData[i])
+        }
+    }
+    res.json({"message": "User does not exist"})
+});
+
+// get random users - count in URL
+app.get("/random-users/:count", (req, res)=>{
+    
 });
 
 
--- GetAPIs/Python/main.py
+++ GetAPIs/Python/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+import json
 app = FastAPI()
+
+f = open('data.json')
+theData = json.load(f)
 
 
 @app.get("/") # declare a route, type (GET, POST)
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Hello"}
+
+# get all the users
+@app.get("/all-users")
+def allUsers():
+    return theData
+
+# get a particular user
+@app.get("/user/{id}")
+def particularUser(id: int):
+    for d in theData:
+        if d['id'] == id:
+            return d
+    return {"message": "User does not exist"}
+
+
+# get random users - count in URL
+@app.get("/random-users/{count}")
+def randomUsers(count: int):
+    return {"message": "User does not exist"}
