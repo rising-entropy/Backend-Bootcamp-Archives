8e90716665e9569ed0b0559d57f850fcc06c2c76
Devang <risingentropy20@gmail.com>
2021-12-30 21:25:17 +0530

    some

---
+++ TokenAuth/Node/index.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const app = express();
+
+const theData = require("./data.json");
+
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.json({message: "Hello"})
+});
+
+// get all the users
+app.get("/all-users", (req, res)=>{
+    res.json(theData)
+})
+
+// get a particular user
+app.get("/user/:id", (req, res)=>{
+    let id = parseInt(req.params.id);
+    for(let i=0; i<theData.length; i++)
+    {
+        if(theData[i].id === id)
+        {
+            res.json(theData[i])
+        }
+    }
+    res.json({"message": "User does not exist"})
+});
+
+const getRandomIndicesArray = (countOfList, requiredCount) => {
+    let requiredIndices = [];
+    while(true)
+    {
+        let aRandomNumber = Math.floor(Math.random() * countOfList)+1;
+        if(!requiredIndices.includes(aRandomNumber))
+        {
+            requiredIndices.push(aRandomNumber);
+        }
+        if(requiredIndices.length == requiredCount)
+        {
+            return requiredIndices;
+        }
+    }
+}
+
+// get random users - count in URL
+app.get("/random-users/:count", (req, res)=>{
+    let theCount = req.params.count;
+    let theListCount = theData.length;
+    let theRandomIndices = getRandomIndicesArray(theListCount, theCount);
+    
+    let requiredObjects = []
+    for(let i=0; i<theRandomIndices.length; i++)
+    {
+        requiredObjects = [...requiredObjects, theData[theRandomIndices[i]]]
+        //requiredObjects.push(theData[theRandomIndices[i]]);
+    }
+    res.json(requiredObjects)
+});
+
+// post - create a new user
+app.post("/create-user", (req, res)=>{
+    let theNewUser = req.body;
+    theNewUser["id"] = theData.length+1;
+    theData.push(theNewUser);
+    res.json(theNewUser);
+});
+
+// put - update a user (getting its ID)
+app.put("/user/:id", (req, res)=>{
+    let id = parseInt(req.params.id);
+    let theBody = req.body;
+    for(let i=0; i<theData.length; i++)
+    {
+        if(theData[i].id === id)
+        {
+            let theUpdatedUser = theBody;
+            theUpdatedUser["id"] = id;
+            theData[i] = theUpdatedUser;
+            res.json(theUpdatedUser);
+        }
+    }
+    res.json({"message": "User does not exist"});
+});
+
+// delete - delete a user (getting its ID)
+app.delete("/user/:id", (req, res)=>{
+    let id = parseInt(req.params.id);
+    theData.filter((ele, index)=>{
+        if(ele.id === id)
+        {
+            theData.splice(index, 1);
+            res.json({
+                message: "User deleted successfully."
+            });
+        }
+    });
+    res.json({"message": "User does not exist"});
+})
+
+app.listen(3000, ()=>{
+    console.log("Server Started");
+});
+
+
+// Login API - Validate the User
+// If authenticated - Return a Token
+
+
+// Check Token API
+// Return True if valid else False
---
+++ TokenAuth/Python/main.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI
+import json
+import random
+app = FastAPI()
+from pydantic import BaseModel
+
+f = open('data.json')
+theData = json.load(f)
+
+
+@app.get("/") # declare a route, type (GET, POST)
+def read_root():
+    return {"message": "Hello"}
+
+# get all the users
+@app.get("/all-users")
+def allUsers():
+    return theData
+
+# get a particular user
+@app.get("/user/{id}")
+def particularUser(id: int):
+    for d in theData:
+        if d['id'] == id:
+            return d
+    return {"message": "User does not exist"}
+
+def getRandomIndicesArray(countOfList, requiredCount):
+    requiredIndices = []
+    while True:
+        r1 = random.randint(1, countOfList)
+        if r1 not in requiredIndices:
+            requiredIndices.append(r1)
+        if len(requiredIndices) == requiredCount:
+            return requiredIndices
+
+# get random users - count in URL
+@app.get("/random-users/{count}")
+def randomUsers(count: int):
+    theRandomIndices = getRandomIndicesArray(len(theData), count)
+    requiredObjects = []
+    for i in theRandomIndices:
+        requiredObjects.append(theData[i])
+    return requiredObjects
+
+# post - create a new user
+class User(BaseModel):
+    firstName: str
+    lastName: str
+    email: str
+    password: str
+
+@app.post("/create-user")
+def newUser(user: User):
+    user = dict(user)
+    user["id"] = len(theData)+1
+    theData.append(user)
+    return user
+
+# put - update a user (getting its ID)
+@app.put("/user/{id}")
+def updateUser(id: int, user: User):
+    for i in range(len(theData)):
+        if theData[i]["id"] == id:
+            # make a new copy of that updated instance
+            theUpdatedUser = dict(user)
+            # pass the same ID
+            theUpdatedUser["id"] = id
+            #put it back in place of old instance
+            theData[i] = theUpdatedUser
+            return theUpdatedUser
+    return {"message": "User does not exist"}
+            
+
+# delete - delete a user (getting its ID)
+@app.delete("/user/{id}")
+def deleteUser(id: int):
+    for i in range(len(theData)):
+        if theData[i]["id"] == id:
+            del theData[i]
+            return {
+                "message": "User deleted successfully."
+            }
+    return {"message": "User does not exist"}
+
+# Login API - Validate the User
+# If authenticated - Return a Token
+
+
+# Check Token API
+# Return True if valid else False
